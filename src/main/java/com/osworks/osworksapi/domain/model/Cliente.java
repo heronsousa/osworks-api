9368c62e4feed1f41fabd9d447c0fd5b1d16381b
heronsousa <heronrs1@gmail.com>
2020-04-21 15:54:43 -0300

    creating cliente model

---
+++ src/main/java/com/osworks/osworksapi/domain/model/Cliente.java
@@ -0,0 +1,43 @@
+package com.osworks.osworksapi.domain.model;
+
+public class Cliente {
+
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
