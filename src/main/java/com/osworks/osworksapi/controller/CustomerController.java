4d7697f6dda6f2a310d37466c8b01f0b77399338
heronsousa <heronrs1@gmail.com>
2020-04-23 12:46:59 -0300

    creating customer repository

--- src/main/java/com/osworks/osworksapi/controller/CustomerController.java
+++ src/main/java/com/osworks/osworksapi/controller/CustomerController.java
@@ -1,24 +1,23 @@
 package com.osworks.osworksapi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import com.osworks.osworksapi.domain.model.Customer;
+import com.osworks.osworksapi.domain.repository.CustomerRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
     
-    @PersistenceContext
-    private EntityManager manager;
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @GetMapping("/customers")
     public List<Customer> listar() {
-        return manager.createQuery("from Customer", Customer.class).getResultList();
+        return customerRepository.findAll();
+        // return customerRepository.findByName("Heron");
     }
 }
---
+++ src/main/java/com/osworks/osworksapi/domain/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package com.osworks.osworksapi.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.osworks.osworksapi.domain.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    
+    List<Customer> findByName(String name);
+}
