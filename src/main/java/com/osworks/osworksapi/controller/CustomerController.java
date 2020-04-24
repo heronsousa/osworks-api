1617cbbcf4a6f8656b8ceeab4d440a471296189f
heronsousa <heronrs1@gmail.com>
2020-04-24 19:22:33 -0300

    adding route update customer

--- src/main/java/com/osworks/osworksapi/controller/CustomerController.java
+++ src/main/java/com/osworks/osworksapi/controller/CustomerController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -48,4 +49,16 @@ public class CustomerController {
         return customerRepository.save(customer);
     }
     
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Customer> update(@PathVariable Long customerId, @RequestBody Customer customer) {
+        if(!customerRepository.existsById(customerId)){
+            return ResponseEntity.notFound().build();
+        }
+        
+        customer.setId(customerId);
+        customerRepository.save(customer);
+
+        return ResponseEntity.ok(customer);
+    }
 }
