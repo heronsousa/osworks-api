9bf5dcaf5e1c3714fa0203b315104d06efe69b8f
heronsousa <heronrs1@gmail.com>
2020-04-24 19:10:40 -0300

    adding route create customer

--- src/main/java/com/osworks/osworksapi/controller/CustomerController.java
+++ src/main/java/com/osworks/osworksapi/controller/CustomerController.java
@@ -7,11 +7,16 @@ import com.osworks.osworksapi.domain.model.Customer;
 import com.osworks.osworksapi.domain.repository.CustomerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/customers")
@@ -35,4 +40,12 @@ public class CustomerController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer create(@RequestBody Customer customer) {
+        
+        return customerRepository.save(customer);
+    }
+    
 }
