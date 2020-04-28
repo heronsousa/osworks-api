53ce4c52fbb6c0c6b9f8e269c7ebb15d70cf5d4f
heronsousa <heronrs1@gmail.com>
2020-04-28 20:52:48 -0300

    creating table service order

---
+++ src/main/resources/db/migration/V002__create-table-service-order.sql
@@ -0,0 +1,14 @@
+CREATE TABLE service_order (
+	id bigint not null auto_increment,
+    customer_id bigint not null,
+    description text not null,
+    price decimal(10,2) not null,
+    status varchar(20) not null,
+    opening_date datetime not null,
+    closing_date datetime not null,
+    
+    primary key (id)
+);
+
+ALTER TABLE service_order ADD CONSTRAINT fk_service_order_customer
+FOREIGN KEY (customer_id) REFERENCES customer (id)
