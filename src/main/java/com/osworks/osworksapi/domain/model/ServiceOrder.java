c12a7575000f2ae2a3b2d19276c5c1e93aa27d94
heronsousa <heronrs1@gmail.com>
2020-05-01 12:10:05 -0300

    creating comment model

--- src/main/java/com/osworks/osworksapi/domain/model/ServiceOrder.java
+++ src/main/java/com/osworks/osworksapi/domain/model/ServiceOrder.java
@@ -2,6 +2,8 @@ package com.osworks.osworksapi.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class ServiceOrder {
@@ -29,6 +32,9 @@ public class ServiceOrder {
 
     private OffsetDateTime openingDate;
     private OffsetDateTime closingDate;
+
+    @OneToMany(mappedBy = "serviceOrder")
+    private List<Comment> comments = new ArrayList<>();
 
     public Long getId() {
         return this.id;
@@ -84,7 +90,39 @@ public class ServiceOrder {
 
     public void setClosingDate(OffsetDateTime closingDate) {
         this.closingDate = closingDate;
-    };
+    }
 
+    public List<Comment> getComments() {
+        return this.comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ServiceOrder other = (ServiceOrder) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
     
