d6b489cc873681860ce2874e3044026119520d1a
heronsousa <heronrs1@gmail.com>
2020-05-01 12:21:04 -0300

    adding method addComment in service order managemment

--- src/main/java/com/osworks/osworksapi/domain/service/ServiceOrderManagementService.java
+++ src/main/java/com/osworks/osworksapi/domain/service/ServiceOrderManagementService.java
@@ -3,9 +3,11 @@ package com.osworks.osworksapi.domain.service;
 import java.time.OffsetDateTime;
 
 import com.osworks.osworksapi.domain.exception.ServiceException;
+import com.osworks.osworksapi.domain.model.Comment;
 import com.osworks.osworksapi.domain.model.Customer;
 import com.osworks.osworksapi.domain.model.ServiceOrder;
 import com.osworks.osworksapi.domain.model.ServiceOrderStatus;
+import com.osworks.osworksapi.domain.repository.CommentRepository;
 import com.osworks.osworksapi.domain.repository.CustomerRepository;
 import com.osworks.osworksapi.domain.repository.ServiceOrderRepository;
 
@@ -21,6 +23,9 @@ public class ServiceOrderManagementService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private CommentRepository commentRepository;
+
     public ServiceOrder create(ServiceOrder serviceOrder) {
         Customer customer = customerRepository.findById(serviceOrder.getCustomer().getId())
                             .orElseThrow(() -> new ServiceException("Cliente não encontrado."));
@@ -30,5 +35,18 @@ public class ServiceOrderManagementService {
         serviceOrder.setOpeningDate(OffsetDateTime.now());
 
         return serviceOrderRepository.save(serviceOrder);
+    }
+
+    public Comment addComment(Long serviceOrderId, String description) {
+
+        ServiceOrder serviceOrder = serviceOrderRepository.findById(serviceOrderId)
+                            .orElseThrow(() -> new ServiceException("Ordem de serviçop não encontrada."));
+
+        Comment comment = new Comment();
+        comment.setDescription(description);
+        comment.setSendDate(OffsetDateTime.now());
+        comment.setServiceOrder(serviceOrder);
+
+        return commentRepository.save(comment);
     }
 }
